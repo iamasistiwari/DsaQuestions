37470c49da33be1d8732e60487de2f9ffbba34d5
iamasistiwari <iamashishtiwari2004@gmail.com>
2024-06-04 19:22:07 +0530

    quesion added

--- Q3.java
+++ Q3.java
@@ -1,4 +1,7 @@
 package DsaQuestions;
 
 public class Q3 {
+    public static void main(String[] args) {
+        System.out.print("Hi my name is hacker");
+    }
 }
---
+++ Q4.java
@@ -0,0 +1,2 @@
+package DsaQuestions;public class Q4 {
+}
